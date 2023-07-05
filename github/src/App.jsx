459f7399db9825ed2776592829e7bec13dfc7fce
Mount-Blanc <mountblanc100@gmail.com>
2023-07-04 18:44:11 -0700

    initialized project

---
+++ github/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <header></header>
+   <main>
+    <section></section>
+    <secction></secction>
+    <section></section>
+    </main>  
+    </>
+  )
+}
+
+export default App
