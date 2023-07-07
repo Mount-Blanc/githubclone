7c655f4e0be984985f247565a24bd7322fd3d8fd
Mount-Blanc <mountblanc100@gmail.com>
2023-07-07 11:49:02 -0700

    added 2 sections

--- github/src/App.jsx
+++ github/src/App.jsx
@@ -20,8 +20,43 @@ function App() {
     </nav>
    </header>
    <main>
-    <section></section>
-    <secction></secction>
+    <section>
+      <div>
+        <div>
+          <h3>Top Repositories</h3>
+          <button>New</button>
+          <input placeholder='Find a repository...'/>
+        </div>
+        <div>
+          <p>User/Repository</p>
+        </div>
+        <div>
+          <h3>Recent activity</h3>
+          <div>
+            <p>When you take actions across GitHub, we'll provide
+              links to that activity here.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <secction>
+      <div>
+        <h3>For you</h3>
+        <h3>Following</h3>
+        <div>
+          <p>Send feedback</p>
+          <p>Filter</p>
+        </div>
+      </div>
+      <div>
+        <h3>Welcome to the new feed !</h3>
+        <p>We're updating the cards and ranking all the time , so check back
+          regularly. At first, you might need to follow some people or star some Repositories to get started
+
+        </p>
+      </div>
+    </secction>
     <section></section>
     </main>  
     </>
